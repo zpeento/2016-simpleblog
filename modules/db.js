92964372ce7f6cbfed273d5fa68515570b667603
iceiceice <509887158@qq.com>
2016-10-27 17:30:58 +0800

    [add]mongodb数据库配置

---
+++ modules/db.js
@@ -0,0 +1,12 @@
+// var settings = require('../settings');
+// var Db = require('mongodb').Db;
+// var connection = require('mongodb').Connection;
+// var Server = require('mongodb').Server;
+
+// module.exports = new Db(settings.db,new Server(settings.host,settings.port),{safe: true});
+var settings = require('../settings'),
+        Db = require('mongodb').Db,
+        Connection = require('mongodb').Connection,
+        Server = require('mongodb').Server;
+    module.exports = new Db(settings.db, new Server(settings.host, settings.port),
+ {safe: true});
