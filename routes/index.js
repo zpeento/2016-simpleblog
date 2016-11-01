f5711cc7d38388eb7887aa26ba00a72f7c598bd9
iceiceice <509887158@qq.com>
2016-11-01 15:32:54 +0800

    [update]添加发表文章功能

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto');
 var User = require('../modules/user')
+var Post = require('../modules/post')
 var express = require('express');
 var router = express.Router();
 
@@ -9,19 +10,47 @@ var router = express.Router();
 // });
 
 // module.exports = router;
+    //检测是否登录
+    function checkLogin(req, res, next) {
+      if (!req.session.user){
+        req.flash('error','未登录');
+        res.redirect('/login');
+      }
+      next();
+    }
+
+    //检测是否未登录
+    function checkNotLogin(req, res, next) {
+      if (req.session.user) {
+        req.flash('error','已登录');
+        res.redirect('back');//返回之前页面
+      }
+      next();
+    }
 
 
 module.exports = function(app) {
   	//首页get
   	app.get('/',function(req,res){
-  		var data = {
-  			title:	'主页',
-  			user: req.session.user,
-        success: req.flash('success').toString(),
-        error:req.flash('error').toString()
-
-  		};
-  		res.render('index',data);
+      Post.get(null,function(err,posts){
+        if(err){
+          posts = [];
+        }
+        res.render('index',{
+          title:'主页',
+          user:req.session.user,
+          posts:posts,
+          success:req.flash('success').toString(),
+          error:req.flash('error').toString()
+        })
+      })
+  		// var data = {
+  		// 	title:	'主页',
+  		// 	user: req.session.user,
+    //     success: req.flash('success').toString(),
+    //     error:req.flash('error').toString()
+  		// };
+  		// res.render('index',data);
   	});
 
   	//注册页get
@@ -29,9 +58,9 @@ module.exports = function(app) {
   	app.get('/reg', function (req, res) {
     	var data = {
         title:'注册',
-        user: req.session.user,
-        success: req.flash('success').toString(),
-        error:req.flash('error').toString()
+        // user: req.session.user,
+        // success: req.flash('success').toString(),
+        // error:req.flash('error').toString()
       }
       res.render('reg', data);
   	});
@@ -45,7 +74,7 @@ module.exports = function(app) {
       var password_repeat = req.body.password_repeat;
       //检验两次密码是否一致
       if(password != password_repeat){
-        console.log(0);
+        // console.log(0);
         // alert(0);
         req.flash('error','两次输入的密码不一样！');
         return res.redirect('/reg');//返回注册页
@@ -115,6 +144,8 @@ module.exports = function(app) {
         res.redirect('/');
       })
   	});
+
+    //post/get
     app.get('/post', checkLogin)
  	  app.get('/post', function (req, res) {
     	var data = {
@@ -125,9 +156,23 @@ module.exports = function(app) {
       }
       res.render('post', data);
   	});
+
+    //post/post
     app.post('/post', checkLogin)
   	app.post('/post', function (req, res) {
+      var currentUser = req.session.user;
+      var post = new Post(currentUser.name,req.body.title,req.body.post);
+      post.save(function(err){
+        if(err) {
+          req.flash('error',err);
+          return res.direct('/');
+        }
+        req.flash('success','发布成功');
+        res.redirect('/');
+      })
   	});
+
+    //logout/get
     app.get('/logout', checkLogin)
   	app.get('/logout', function (req, res) {
       req.session.user = null;
@@ -136,18 +181,5 @@ module.exports = function(app) {
   	});
 
 
-    function checkLogin(req, res, next) {
-      if (!req.session.user){
-        req.flash('error','未登录');
-        res.redirect('/login');
-      }
-      next();
-    }
-    function checkNotLogin(req, res, next) {
-      if (req.session.user) {
-        req.flash('error','已登录');
-        res.redirect('back');//返回之前页面
-      }
-      next();
-    }
+    
 };
